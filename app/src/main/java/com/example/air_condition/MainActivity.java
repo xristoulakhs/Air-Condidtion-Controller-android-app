dcca0e267b0b584f2e28b144679a59e30a791c5b
xristoulakhs <antonisxania55@gmail.com>
2023-03-31 17:22:32 +0300

    init project

---
+++ app/src/main/java/com/example/air_condition/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.air_condition;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.example.air_condition.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}

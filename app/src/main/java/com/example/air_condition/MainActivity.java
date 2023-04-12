554a8854e50059e3d03eb968c8f8b86832a3248a
xristoulakhs <antonisxania55@gmail.com>
2023-04-12 13:14:59 +0300

    changes at home ui

--- app/src/main/java/com/example/air_condition/MainActivity.java
+++ app/src/main/java/com/example/air_condition/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.air_condition;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -12,10 +18,20 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
+    LinearLayout fanBtn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        fanBtn = findViewById(R.id.fanButton);
 
+        fanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }

d337208acb850684a1202d2fe4d57f6e25f010d1
JimmyHtv <jimhoxh1@gmail.com>
2023-04-14 16:13:50 +0300

    help button

---
+++ app/src/main/java/com/example/air_condition/HelpActivity.java
@@ -0,0 +1,16 @@
+package com.example.air_condition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class HelpActivity extends AppCompatActivity {
+    ImageView help_button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
+

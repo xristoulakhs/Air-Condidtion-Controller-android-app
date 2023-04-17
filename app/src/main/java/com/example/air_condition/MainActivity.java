50af88c7a29996a57395ac869b494cec42d84515
xristoulakhs <antonisxania55@gmail.com>
2023-04-17 13:12:32 +0300

    homepage ui done

--- app/src/main/java/com/example/air_condition/MainActivity.java
+++ app/src/main/java/com/example/air_condition/MainActivity.java
@@ -6,7 +6,9 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
     LinearLayout fanBtn;
+    LinearLayout directionBtn;
+    LinearLayout modeBtn;
+    LinearLayout ecoBtn;
+    LinearLayout autoBtn;
+    LinearLayout sleepBtn;
+
+    LinearLayout powerBtn;
+
+    TextView statusText;
+    ImageView statusImg;
 
 
     @Override
@@ -27,6 +39,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         fanBtn = findViewById(R.id.fanButton);
+        directionBtn = findViewById(R.id.directionButton);
+        modeBtn = findViewById(R.id.modeButton);
+        ecoBtn = findViewById(R.id.ecoButton);
+        autoBtn = findViewById(R.id.autoButton);
+        sleepBtn = findViewById(R.id.sleepButton);
+
+        powerBtn = findViewById(R.id.onOffBtn);
+
+        statusText = findViewById(R.id.statusText);
+        statusImg = findViewById(R.id.statusIcon);
 
         fanBtn.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("RestrictedApi")

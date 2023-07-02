910320978e08b44d1b0818f1ed1be5baf6ac1c70
jimho <jimhoxh1@gmail.com>
2023-07-02 22:57:37 +0300

    change language

--- app/src/main/java/com/example/air_condition/SettingsActivity.java
+++ app/src/main/java/com/example/air_condition/SettingsActivity.java
@@ -4,13 +4,21 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.AdapterView;
+
+import android.content.res.Resources;
+import android.content.res.Configuration;
+import java.util.Locale;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -38,6 +46,7 @@ public class SettingsActivity extends AppCompatActivity {
     Switch vibrationSwitch;
     Switch speechSwitch;
 
+    ImageButton editDevicesButton;
     Vibrator vibrator;
 
     @Override
@@ -53,27 +62,40 @@ public class SettingsActivity extends AppCompatActivity {
         timerMinus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(DAO.isVibrationState()) vibrator.vibrate(100);
-                int txtNum = Integer.parseInt(DAO.getTimerText().split(" ",2)[0]);
-                if(txtNum>0){
+                if (DAO.isVibrationState()) vibrator.vibrate(100);
+                int txtNum = Integer.parseInt(DAO.getTimerText().split(" ", 2)[0]);
+                if (txtNum > 0) {
                     timerTxt.setText(txtNum - 5 + " mins");
-                    DAO.setTimerText(txtNum - 5 +" mins");
+                    DAO.setTimerText(txtNum - 5 + " mins");
                 }
             }
         });
 
-        timerPlus =findViewById(R.id.timerPlus);
+        timerPlus = findViewById(R.id.timerPlus);
         timerPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(DAO.isVibrationState()) vibrator.vibrate(100);
-                int txtNum = Integer.parseInt(DAO.getTimerText().split(" ",2)[0]);
-                if(txtNum<100){
+                if (DAO.isVibrationState()) vibrator.vibrate(100);
+                int txtNum = Integer.parseInt(DAO.getTimerText().split(" ", 2)[0]);
+                if (txtNum < 100) {
                     timerTxt.setText(txtNum + 5 + " mins");
-                    DAO.setTimerText(txtNum + 5 +" mins");
+                    DAO.setTimerText(txtNum + 5 + " mins");
                 }
             }
         });
+
+        // Retrieve the languageSpinner from the layout
+        languageSpinner = findViewById(R.id.languageSpinner);
+
+        // Create an array of language options
+        String[] languages = {"English", "Greek"};
+
+        // Create an ArrayAdapter to populate the spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, languages);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Set the adapter on the spinner
+        languageSpinner.setAdapter(adapter);
 
         devicesSpinner = findViewById(R.id.myDevicesSpinner);
         ArrayList<String> devices = new ArrayList<>();
@@ -84,11 +106,26 @@ public class SettingsActivity extends AppCompatActivity {
         devicesArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         devicesSpinner.setAdapter(devicesArrayAdapter);
 
+        languageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                String selectedLanguage = adapterView.getItemAtPosition(position).toString();
+                setAppLanguage(selectedLanguage);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // Do nothing
+            }
+        });
+
+
+
         turboSwitch = findViewById(R.id.turboSwitch);
         turboSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(DAO.isVibrationState()) vibrator.vibrate(100);
+                if (DAO.isVibrationState()) vibrator.vibrate(100);
                 DAO.setTurboState(!DAO.isTurboState());
             }
         });
@@ -97,7 +134,7 @@ public class SettingsActivity extends AppCompatActivity {
         vibrationSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(DAO.isVibrationState()) vibrator.vibrate(100);
+                if (DAO.isVibrationState()) vibrator.vibrate(100);
                 DAO.setVibrationState(!DAO.isVibrationState());
             }
         });
@@ -106,7 +143,7 @@ public class SettingsActivity extends AppCompatActivity {
         speechSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(DAO.isVibrationState()) vibrator.vibrate(100);
+                if (DAO.isVibrationState()) vibrator.vibrate(100);
                 DAO.setSpeechState(!DAO.isSpeechState());
             }
         });
@@ -115,7 +152,7 @@ public class SettingsActivity extends AppCompatActivity {
         rb_cel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(DAO.isVibrationState()) vibrator.vibrate(100);
+                if (DAO.isVibrationState()) vibrator.vibrate(100);
                 DAO.setCelcius(true);
             }
         });
@@ -124,15 +161,14 @@ public class SettingsActivity extends AppCompatActivity {
         rb_fahr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(DAO.isVibrationState()) vibrator.vibrate(100);
+                if (DAO.isVibrationState()) vibrator.vibrate(100);
                 DAO.setCelcius(false);
             }
         });
 
-        if(DAO.isCelcius()){
+        if (DAO.isCelcius()) {
             rb_cel.setChecked(true);
-        }
-        else {
+        } else {
             rb_fahr.setChecked(true);
         }
 
@@ -140,6 +176,26 @@ public class SettingsActivity extends AppCompatActivity {
         vibrationSwitch.setChecked(DAO.isVibrationState());
         speechSwitch.setChecked(DAO.isSpeechState());
         timerTxt.setText(DAO.getTimerText());
+
+        editDevicesButton = findViewById(R.id.editDevicesButton);
+        editDevicesButton.setVisibility(View.GONE);
+
+        devicesSpinner.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                // Show the "Edit Devices" button when the spinner is touched
+                editDevicesButton.setVisibility(View.VISIBLE);
+                return false;
+            }
+        });
+
+        editDevicesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle the click event of the "Edit Devices" button
+                // Add your logic here
+            }
+        });
     }
 
     @Override
@@ -147,4 +203,25 @@ public class SettingsActivity extends AppCompatActivity {
         setResult(RESULT_OK);
         finish();
     }
+
+
+    private void setAppLanguage(String language) {
+        // Get the application's resources
+        Resources resources = getResources();
+        Configuration configuration = resources.getConfiguration();
+
+        // Create a locale object based on the selected language
+        Locale locale = new Locale(language);
+
+        // Update the configuration with the new locale
+        configuration.setLocale(locale);
+
+        // Update the resources with the new configuration
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+
+        // Restart the activity to apply the language change
+        recreate();
+    }
+
+
 }
